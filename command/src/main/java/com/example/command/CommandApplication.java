cd3f8eed545081eed114a85968cd51425ec148b5
practise30 <practise30@gmail.com>
2024-05-14 16:13:46 +0900

    feat : axon설치/postgresql 연결

---
+++ command/src/main/java/com/example/command/CommandApplication.java
@@ -0,0 +1,18 @@
+package com.example.command;
+
+
+import org.axonframework.commandhandling.CommandHandler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Objects;
+
+@SpringBootApplication
+public class CommandApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommandApplication.class,args);
+    }
+
+    @CommandHandler
+    protected void test(Object o) {}
+}
